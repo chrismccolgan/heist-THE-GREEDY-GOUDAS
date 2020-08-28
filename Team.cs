0a3a7b7bab3bcdc4b9b49d053d9b5d65c3177402
Tasha Lane <designtmg77@gmail.com>
2020-08-28 15:05:54 -0500

    phase3 completed

--- Team.cs
+++ Team.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace heist_goudas
 {
@@ -16,12 +17,32 @@ namespace heist_goudas
         }
         public void DisplayMembers()
         {
+            Console.WriteLine();
+            Console.WriteLine("-------------");
+            Console.WriteLine("THE TEAM:");
+            Console.WriteLine("-------------");
             foreach (TeamMember person in AllTeamMembers)
             {
                 Console.WriteLine($"NAME: {person.Name}");
                 Console.WriteLine($"SKILL LEVEL: {person.SkillLevel}");
                 Console.WriteLine($"COURAGEOUSNESS: {person.CourageFactor}");
                 Console.WriteLine();
+            }
+        }
+
+        ///Sum the skill levels of the team. Save that number.
+        public void SkillSum(int bankLevel)
+        {
+            int totalSkill = AllTeamMembers.Sum(memberSkill => memberSkill.SkillLevel);
+            Console.WriteLine($"Your team's total skill level: {totalSkill}");
+            Console.WriteLine($"The Bank's Difficulty: {bankLevel}");
+            if (totalSkill >= bankLevel)
+            {
+                Console.WriteLine("Congratulations, you have successfully robbed a bank.  I hope you feel good about yourself, I guess...");
+            }
+            else
+            {
+                Console.WriteLine("Hope you have a good lawyer...");
             }
         }
     }
