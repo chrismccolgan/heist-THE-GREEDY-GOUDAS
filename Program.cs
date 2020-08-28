7ab5442586179f398a3530253567d80a7e4a3943
Brendan Abernethy <brendan.j.abernethy@gmail.com>
2020-08-28 15:44:02 -0500

    cleaned up formatting

--- Program.cs
+++ Program.cs
@@ -43,16 +43,24 @@ namespace heist_goudas
             //After the user enters the team information, prompt them to enter the number of trial runs the program should perform.
 
             Console.Write("How many test runs? ");
+
             string StringRuns = Console.ReadLine();
+            Console.WriteLine();
+            Console.WriteLine("------------");
             int runs = Int32.Parse(StringRuns);
             for (int i = 0; i < runs; i++)
             {
 
-                int luck = new Random().Next(-10, 10);
-                Console.WriteLine(luck);
-                int BankDifficulty = 100 + luck;
+                Console.WriteLine();
+                Console.WriteLine("-----");
 
+                int luck = new Random().Next(-10, 10);
+                Console.WriteLine($"Your luck: {luck}");
+                int BankDifficulty = 100 + luck;
                 newTeam.SkillSum(BankDifficulty);
+
+                Console.WriteLine("-----");
+                Console.WriteLine();
             }
 
         }
