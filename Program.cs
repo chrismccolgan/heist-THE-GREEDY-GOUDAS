206523d183579e542f79c56f61a4e1890d890f4a
Nicholas Glover <nickglovdev@gmail.com>
2020-08-28 11:24:13 -0500

    Completed Phase 1 with getting the user information

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace heist_goudas
 {
@@ -6,7 +7,26 @@ namespace heist_goudas
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Plan Your Heist!");
+
+            Console.Write("What is your name? ");
+            string name = Console.ReadLine();
+
+            Console.Write("What is your skill level? ");
+            string stringSkill = Console.ReadLine();
+            int skill = Int32.Parse(stringSkill);
+
+            Console.Write("How courageous are you? ");
+            string stringCourage = Console.ReadLine();
+            double courage = double.Parse(stringCourage);
+
+            TeamMember firstMember = new TeamMember(name);
+            firstMember.SkillLevel = skill;
+            firstMember.CourageFactor = courage;
+
+            firstMember.DisplayTeamMember(firstMember);
         }
+
     }
+
 }
